774848771352d35d487c86a5a14c27b2f0493f8a
Eduardo Loo <eduardo.loo@pucp.pe>
2022-05-25 16:15:55 -0500

    Component Extend Sytle -listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -4,7 +4,12 @@ import { ProductContext } from "./ProductCard";
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductImage = ( { img = ''} ) => {
+export interface Props {
+    img?: string;
+    className?: string;
+    style?: React.CSSProperties;
+}
+export const ProductImage = ( { img = '', className, style }: Props ) => {
     const { product } = useContext( ProductContext );
     let imgToShow: string;
     if ( img ) {
@@ -15,6 +20,10 @@ export const ProductImage = ( { img = ''} ) => {
         imgToShow = noImage;
     }
     return (
-        <img className={ styles.productImg} src={ imgToShow } alt="ProductImage" />
+        <img 
+            className={ `${ styles.productImg} ${ className }` }
+            style={ style }
+            src={ imgToShow }
+            alt="ProductImage" />
     );
 }
